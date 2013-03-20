3524872db93711f7101194b94ca42af5d1fdfb0a
Carlos Jesus Fernandez Basso <cjferba@gmail.com>
2013-03-20 18:02:19 +0100

    BD y clase de conexión

--- code/BMA/src/bma/BaseDatos.java
+++ code/BMA/src/bma/BaseDatos.java
@@ -3,49 +3,66 @@
  * and open the template in the editor.
  */
 package bma;
+
 import java.sql.*;
+
 /**
  *
  * @author Francisco
  */
 public class BaseDatos {
-    Connection conexion;   
-    Statement stmt;      
+
+    Connection conexion;
+    Statement stmt;
     ResultSet retset;
-    
-    public Connection abreConexion(String driver,String servidor, String usuario, String clave) {
-        try{
+
+    public BaseDatos(String driver, String servidor, String usuario, String clave) {
+        try {
             Class.forName(driver);
             //Busca el controlador y abre conexion
-            return DriverManager.getConnection(servidor,usuario,clave);
-        }catch(ClassNotFoundException e){
+        } catch (ClassNotFoundException e) {
+            System.err.print("No se han podido cargar los controladores"); 
+        }/* catch (SQLException e) {
+            System.err.print("No se ha podido acceder a la base de datos");
+            
+        }*/
+
+    }
+
+    public Connection abreConexion(String driver, String servidor, String usuario, String clave) {
+        try {
+            Class.forName(driver);
+            //Busca el controlador y abre conexion
+            return DriverManager.getConnection(servidor, usuario, clave);
+        } catch (ClassNotFoundException e) {
             System.out.print("No se han podido cargar los controladores");
             return null;
-        }catch(SQLException e){
+        } catch (SQLException e) {
             System.out.print("No se ha podido acceder a la base de datos");
             return null;
         }
     }
-    public void conectaBD(){
-        conexion = abreConexion("com.mysql.jdbc.Driver","jdbc:mysql://localhost:3306/mydb", "root", "baloncesto");
+
+    public void conectaBD() {
+        conexion = abreConexion("com.mysql.jdbc.Driver", "jdbc:mysql://localhost:3306/mydb", "root", "baloncesto");
     }
 
-    public ResultSet ejecutaConsulta(String consulta){
-        try{
-            stmt = conexion.createStatement();  
+    public ResultSet ejecutaConsulta(String consulta) {
+        try {
+            stmt = conexion.createStatement();
             retset = stmt.executeQuery(consulta);
-        }catch(SQLException ex){
+        } catch (SQLException ex) {
             System.out.print(ex.getMessage());
         }
         return retset;
     }
-    public void ejecutaActualizacion(String actualizacion){
-        try{
+
+    public void ejecutaActualizacion(String actualizacion) {
+        try {
             stmt = conexion.createStatement();
             stmt.executeUpdate(actualizacion);
-        }catch(SQLException ex){
+        } catch (SQLException ex) {
             System.out.print(ex.getMessage());
-        }        
+        }
     }
-
 }
