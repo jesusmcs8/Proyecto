80f274db9de86423cac1b5db8ee67013e8978e25
Carlos Jesus Fernandez Basso <cjferba@gmail.com>
2013-03-26 18:12:49 +0100

    bd java terminada

--- code/BMA/src/bma/BaseDatos.java
+++ code/BMA/src/bma/BaseDatos.java
@@ -17,18 +17,22 @@ public class BaseDatos {
     ArrayList<Connection> conexion = new ArrayList();
     Statement stmt;
     ResultSet retset;
+    String driver;
+    String servidor;
+    String usuario;
+    String clave;
 
     public BaseDatos() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
+            driver = "com.mysql.jdbc.Driver";
+            servidor = "jdbc:mysql://localhost:3306/mydb";
+            usuario = "root";
+            clave = "baloncesto";
             for (int i = 0; i < 10; i++) {
-                this.conexion.add(DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb",  "root", "baloncesto"));
+                this.conexion.add(DriverManager.getConnection(servidor, usuario, clave));
                 //this.conexion.add(DriverManager.getConnection(servidor, usuario, clave));
             }
-
-            //Busca el controlador y abre conexion
-            //("com.mysql.jdbc.Driver", "jdbc:mysql://localhost:3306/mydb", "root", "baloncesto");
-            //DriverManager.getConnection(servidor, usuario, clave);
 
         } catch (ClassNotFoundException e) {
             System.err.print("No se han podido cargar los controladores");
@@ -37,9 +41,33 @@ public class BaseDatos {
         }
     }
 
-    public BaseDatos(String driver, String servidor, String usuario, String clave) {
+    public boolean reconec() {
         try {
             Class.forName(driver);
+            for (int i = 0; i < 10; i++) {
+                if (conexion.get(i).isClosed()) {
+                    conexion.set(i, DriverManager.getConnection(servidor, usuario, clave));
+                }
+
+            }
+        } catch (ClassNotFoundException e) {
+            System.err.print("No se han podido cargar los controladores");
+            return false;
+        } catch (SQLException ex) {
+            System.out.print(ex.getMessage());
+            return false;
+        }
+
+        return true;
+    }
+
+    public BaseDatos(String driv, String serv, String user, String pass) {
+        try {
+            Class.forName(driv);
+            driver = driv;
+            servidor = serv;
+            usuario = user;
+            clave = pass;
             for (int i = 0; i < 10; i++) {
                 this.conexion.add(DriverManager.getConnection(servidor, usuario, clave));
             }
@@ -55,30 +83,32 @@ public class BaseDatos {
         }
     }
 
-    /*public Connection abreConexion(String driver, String servidor, String usuario, String clave) {
-     try {
-     Class.forName(driver);
-     //Busca el controlador y abre conexion
-     return DriverManager.getConnection(servidor, usuario, clave);
-     } catch (ClassNotFoundException e) {
-     System.out.print("No se han podido cargar los controladores");
-     return null;
-     } catch (SQLException e) {
-     System.out.print("No se ha podido acceder a la base de datos");
-     return null;
-     }
-     }*/
+  
+    public int comprobar() {
+        int i = 0;
+        try {
+            while (conexion.get(i).isClosed()) {
+                i++;
+                if (i > 10) {
+                    reconec();
+                    i = 0;
+                }
+            }
+            if (i != 0) {
+                reconec();
+            }
+        } catch (SQLException ex) {
+            System.out.print(ex.getMessage());
+        }
 
-    /* public void conectaBD() {
-     conexion = abreConexion("com.mysql.jdbc.Driver", "jdbc:mysql://localhost:3306/mydb", "root", "baloncesto");
-     }*/
+        return i;
+    }
+
     public ResultSet ejecutaConsulta(String consulta) {
 
         try {
-            int i = 0;
-            while (!conexion.get(i).isValid(i)) {
-                i++;
-            }
+            int i = comprobar();
+            System.out.println(i);
             stmt = conexion.get(i).createStatement();
             retset = stmt.executeQuery(consulta);
         } catch (SQLException ex) {
